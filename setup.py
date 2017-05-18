a5e0b30312a010aec2e5d5b8632ccd23f3af0968
elan17 <elan17.programacion@gmail.com>
2017-05-18 23:18:07 +0200

    Meaningless push

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from setuptools import setup
 
-setup(name='Crypt_Client',
+setup(name='Crypt_Connection',
       version='1.0',
       description='Socket-based cryptographic server-client',
       author='Juan Toca',
       author_email='elan17.programacion@gmail.com',
       packages=['Crypt_Client', 'Crypt_Server'],
+      provides=['Crypt_Client', 'Crypt_Server'],
       install_requires=['pycryptodome']
       )
